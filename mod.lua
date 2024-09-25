5d0697278bf6837138fbf2d87d71394bc1940e9e
stupxd <uknownvector@gmail.com>
2024-09-25 18:13:30 +0300

    add steamodded metadata

---
+++ mod.lua
@@ -0,0 +1,16 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Blueprint
+--- MOD_ID: blueprint
+--- MOD_AUTHOR: [stupxd aka stupid]
+--- MOD_DESCRIPTION: Dynamically change Blueprint texture 
+--- PRIORITY: 69
+--- BADGE_COLOR: 4B69CF
+--- DISPLAY_NAME: Blueprint
+--- VERSION: 1.0
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+----------------------------------------------
+------------MOD CODE END----------------------
