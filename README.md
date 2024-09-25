c947dc1ca7af3cfec80864e55b6d219fd86584f3
stupxd <uknownvector@gmail.com>
2024-09-25 17:07:02 +0300

    fix sprite alignment, change back to blueprint when dragging. add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+## Requirements
+- [Lovely](https://github.com/ethangreen-dev/lovely-injector) - a Balatro injector.
+
+## Installation
+1. Install [Lovely](https://github.com/ethangreen-dev/lovely-injector?tab=readme-ov-file#manual-installation).
+2. Download the [latest release](https://github.com/stupxd/Blueprint/releases/) of this mod.
+3. Unzip the folder, and move it into the `%appdata%/Balatro/Mods` folder.
+4. Restart the game to load the mod.
+
+## Features
+Dynamically changes blueprint texture to the joker it is copying.
+
--- main.lua
+++ main.lua
@@ -1,3 +1,25 @@
+
+local function align_sprite(self, card, restore)
+    if restore then
+        if self.blueprint_T then
+            self.T.h = self.blueprint_T.h
+            self.T.w = self.blueprint_T.w
+        else
+            self.T.h = G.CARD_H
+            self.T.w = G.CARD_W
+        end
+        return
+    end
+
+    if not self.blueprint_T then
+        self.blueprint_T = {h = self.T.h, w = self.T.w}
+    end
+
+    self.T.h = card.T.h
+    self.T.w = card.T.w
+    self.children.center.scale.y = card.children.center.scale.y
+end
+
 local function blueprint_sprite(blueprint, card)
     if blueprint.blueprint_sprite_key == card.config.center.key then
         return
@@ -5,12 +27,10 @@ local function blueprint_sprite(blueprint, card)
 
     -- Not copying any other joker's sprite at the moment. Cache current sprite before updating
     if not blueprint.blueprint_sprite_copy then
-        print "caching sprite"
         blueprint.blueprint_sprite_copy = blueprint.children.center
     end
 
     blueprint.blueprint_sprite_key = card.config.center.key
-    print("copying card "..card.config.center.key)
 
     blueprint.children.center = Sprite(blueprint.T.x, blueprint.T.y, blueprint.T.w, blueprint.T.h, G.ASSET_ATLAS[card.children.center.atlas.name], card.children.center.sprite_pos)
     blueprint.children.center.states.hover = blueprint.states.hover
@@ -19,6 +39,7 @@ local function blueprint_sprite(blueprint, card)
     blueprint.children.center.states.collide.can = false
     blueprint.children.center:set_role({major = blueprint, role_type = 'Glued', draw_major = blueprint})
 
+    align_sprite(blueprint, card)
 end
 
 local function restore_sprite(blueprint)
@@ -26,12 +47,11 @@ local function restore_sprite(blueprint)
         return
     end
 
-    print("restoring blueprint sprite from "..blueprint.blueprint_sprite_key)
-
     blueprint.children.center:remove()
     blueprint.children.center = blueprint.blueprint_sprite_copy
     blueprint.blueprint_sprite_copy = nil
     blueprint.blueprint_sprite_key = nil
+    align_sprite(blueprint, nil, true)
 end
 
 local cardarea_align_cards = CardArea.align_cards
@@ -44,7 +64,7 @@ function CardArea:align_cards()
         for i = 1, #G.jokers.cards do
             current_joker = G.jokers.cards[i]
             if previous_joker and previous_joker.config and previous_joker.config.center and previous_joker.config.center.key == 'j_blueprint' then
-                if current_joker.config.center.blueprint_compat then
+                if current_joker.config.center.blueprint_compat and not previous_joker.states.drag.is then
                     blueprint_sprite(previous_joker, current_joker)
                 else
                     restore_sprite(previous_joker)
