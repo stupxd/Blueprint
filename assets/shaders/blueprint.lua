e2f4e18a95dbba2b35b829333cac637cbaa321ae
stupxd <uknownvector@gmail.com>
2024-09-26 20:05:45 +0300

    support blueprint editions - apply blueprint shader to atlas copy

--- assets/shaders/blueprint.lua
+++ assets/shaders/blueprint.lua
@@ -1,4 +1,38 @@
-return [[#if defined(VERTEX) || __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
+return [[#if __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
+	#define PRECISION highp
+#else
+	#define PRECISION mediump
+#endif
+
+// cosine based palette, 4 vec3 params
+vec3 palette( float t, vec3 a, vec3 b, vec3 c, vec3 d )
+{
+    return a + b*cos( 6.283185*(c*t+d) );
+}
+
+float lightness(vec4 c)
+{
+	float low = min(c.r, min(c.g, c.b));
+	float high = max(c.r, max(c.g, c.b));
+	float delta = high - low;
+	float sum = high+low;
+
+	return .5 * sum;
+}
+
+vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords )
+{
+    vec4 tex = Texel(texture, texture_coords);
+
+	// 62, 96, 212 (darkest blue) ---- 198, 210, 252 (lightest blue)
+	tex.rgb = palette(lightness(tex), vec3(0.5095,0.6000,0.9095), vec3(0.2665,0.2240,0.0785), vec3(.5,.5,.5), vec3(0.0,0.0,0.0));
+
+	return tex;
+}
+]]
+
+
+--[[return [[#if defined(VERTEX) || __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
 	#define PRECISION highp
 #else
 	#define PRECISION mediump
--- core/core.lua
+++ core/core.lua
@@ -10,8 +10,65 @@ local copy_when_highlighted
 
 --------------------------------------------------
 
+-- Avg blueprint color
+local canvas_background_color = {
+    (62 + 198) / 255 / 2,
+    (96 + 210) / 255 / 2,
+    (212 + 252) / 255 / 2,
+    0
+}
+
+local function process_texture(image)
+    local h, w = image:getDimensions()
+    local canvas = love.graphics.newCanvas(h, w, {type = '2d', readable = true})
+    canvas:setFilter(image:getFilter())
+
+    love.graphics.push()
+    
+    
+    
+    local oldCanvas = love.graphics.getCanvas()
+    local old_filter1, old_filter2 = image:getFilter()
+    
+    love.graphics.setCanvas( canvas )
+    love.graphics.clear(canvas_background_color)
+
+    -- I dont think changing filter does anything.. the image still looks blurry
+    image:setFilter("nearest", "nearest")
+    
+    love.graphics.setShader()
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setShader( G.SHADERS['blueprint_shader'] )
+    canvas:setFilter("nearest", "nearest")
+    
+    -- Draw image with blueprint shader on new canvas
+    love.graphics.draw( image )
 
 
+    love.graphics.setShader()
+    love.graphics.setCanvas(oldCanvas)
+    image:setFilter(old_filter1, old_filter2)
+    canvas:setFilter(image:getFilter())
+
+    love.graphics.pop()
+    return canvas
+end
+
+local function blueprint_atlas(atlas)
+    local blueprinted = atlas.."_blueprinted"
+
+    if not G.ASSET_ATLAS[blueprinted] then
+        G.ASSET_ATLAS[blueprinted] = {}
+        G.ASSET_ATLAS[blueprinted].blueprint = true
+        G.ASSET_ATLAS[blueprinted].name = G.ASSET_ATLAS[atlas].name
+        G.ASSET_ATLAS[blueprinted].type = G.ASSET_ATLAS[atlas].type
+        G.ASSET_ATLAS[blueprinted].px = G.ASSET_ATLAS[atlas].px
+        G.ASSET_ATLAS[blueprinted].py = G.ASSET_ATLAS[atlas].py
+        G.ASSET_ATLAS[blueprinted].image = process_texture(G.ASSET_ATLAS[atlas].image)
+    end
+
+    return G.ASSET_ATLAS[blueprinted]
+end
 
 local function equal_sprites(first, second)
     -- Dynamically update sprite for animated jokers & multiple blueprint copies
@@ -58,7 +115,7 @@ local function blueprint_sprite(blueprint, card)
 
     align_sprite(blueprint, nil, true)
 
-    blueprint.children.center = Sprite(blueprint.T.x, blueprint.T.y, blueprint.T.w, blueprint.T.h, G.ASSET_ATLAS[card.children.center.atlas.name], card.children.center.sprite_pos)
+    blueprint.children.center = Sprite(blueprint.T.x, blueprint.T.y, blueprint.T.w, blueprint.T.h, blueprint_atlas(card.children.center.atlas.name), card.children.center.sprite_pos)
     blueprint.children.center.states.hover = blueprint.states.hover
     blueprint.children.center.states.click = blueprint.states.click
     blueprint.children.center.states.drag = blueprint.states.drag
@@ -66,7 +123,7 @@ local function blueprint_sprite(blueprint, card)
     blueprint.children.center:set_role({major = blueprint, role_type = 'Glued', draw_major = blueprint})
 
     if card.children.floating_sprite then
-        blueprint.children.floating_sprite = Sprite(blueprint.T.x, blueprint.T.y, blueprint.T.w, blueprint.T.h, G.ASSET_ATLAS[card.children.floating_sprite.atlas.name], card.children.floating_sprite.sprite_pos)
+        blueprint.children.floating_sprite = Sprite(blueprint.T.x, blueprint.T.y, blueprint.T.w, blueprint.T.h, blueprint_atlas(card.children.floating_sprite.atlas.name), card.children.floating_sprite.sprite_pos)
         blueprint.children.floating_sprite.role.draw_major = blueprint
         blueprint.children.floating_sprite.states.hover.can = false
         blueprint.children.floating_sprite.states.click.can = false
@@ -104,37 +161,59 @@ local function restore_sprite(blueprint)
 end
 
 return function ()
-    local cardarea_align_cards = CardArea.align_cards
-    function CardArea:align_cards()
-        local ret = cardarea_align_cards(self)
+
+
+local sprite_reset = Sprite.reset
+function Sprite:reset()
+    if self.atlas.blueprint then
+        if type(self.atlas.release) == "function" then
+            self.atlas:release()
+        end
+        self.atlas = blueprint_atlas(self.atlas.name)
+        self:set_sprite_pos(self.sprite_pos)
+        return
+    end
     
-        if self == G.jokers then
-            local previous_joker = nil
-            local current_joker = nil
-            for i = #G.jokers.cards, 1, -1  do
-                current_joker = G.jokers.cards[i]
-                if current_joker.config and current_joker.config.center and current_joker.config.center.key == 'j_blueprint' then
-                    local should_copy = previous_joker and previous_joker.config.center.blueprint_compat and not current_joker.states.drag.is and (copy_when_highlighted or not current_joker.highlighted)
-                    if should_copy and current_joker.edition and not previous_joker.children.floating_sprite then
-                        should_copy = false
-                    end
-                    if should_copy and previous_joker.config.center.key == 'j_blueprint' and not previous_joker.blueprint_sprite_copy then
-                        should_copy = false
-                    end
-    
-                    if should_copy then
-                        blueprint_sprite(current_joker, previous_joker)
-                    else
-                        restore_sprite(current_joker)
-                    end
+    return sprite_reset(self)
+end
+
+
+local cardarea_align_cards = CardArea.align_cards
+function CardArea:align_cards()
+    local ret = cardarea_align_cards(self)
+
+    if self == G.jokers then
+        local previous_joker = nil
+        local current_joker = nil
+        for i = #G.jokers.cards, 1, -1  do
+            current_joker = G.jokers.cards[i]
+            if current_joker.config and current_joker.config.center and current_joker.config.center.key == 'j_blueprint' then
+                local should_copy = previous_joker and previous_joker.config.center.blueprint_compat and not current_joker.states.drag.is and (copy_when_highlighted or not current_joker.highlighted)
+                --if should_copy and current_joker.edition and not previous_joker.children.floating_sprite then
+                --    should_copy = false
+                --end
+                if should_copy and previous_joker.config.center.key == 'j_blueprint' and not previous_joker.blueprint_sprite_copy then
+                    should_copy = false
                 end
-                if not (current_joker.config.center.key == 'j_blueprint') then
-                    previous_joker = current_joker
+
+                if should_copy then
+                    blueprint_sprite(current_joker, previous_joker)
+                else
+                    restore_sprite(current_joker)
                 end
             end
-    
+            if not (current_joker.config.center.key == 'j_blueprint') then
+                previous_joker = current_joker
+            end
         end
-    
-        return ret
+
     end
+
+    return ret
+end
+
+
+
+
+
 end
