487bfacaa78aa61f72870f485d2b4b0cb374a641
stupxd <uknownvector@gmail.com>
2024-12-25 01:25:10 +0200

    v.3.0

--- mod.lua
+++ mod.lua
@@ -2,11 +2,11 @@
 --- MOD_NAME: Blueprint
 --- MOD_ID: blueprint
 --- MOD_AUTHOR: [stupxd aka stupid, Jonathan]
---- MOD_DESCRIPTION: Dynamically change Blueprint & Brainstorm texture.
+--- MOD_DESCRIPTION: Dynamically change Blueprint & Brainstorm textures.
 --- PRIORITY: 69
 --- BADGE_COLOR: 4B69CF
 --- DISPLAY_NAME: Blueprint
---- VERSION: 3.0-wip
+--- VERSION: 3.0
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
