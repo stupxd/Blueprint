638072374db06af5fc176e9cd781931018431d0e
jonathandw743 <jonathandw414@gmail.com>
2025-07-25 20:47:29 +0100

    epic icon

---
+++ smods.lua
@@ -0,0 +1,52 @@
+if SMODS.Atlas then
+    SMODS.Atlas({
+        key = "modicon",
+        path = "justablueguy.png",
+        px = 32,
+        py = 32
+    })
+end
+
+SMODS.DrawStep {
+    key = 'blueprint_sprite_copy',
+    order = 100,
+    func = function(self, layer)
+        if self.blueprint_sprite_copy and self.children.floating_sprite then
+            local scale_mod = 0.07 + 0.02*math.sin(1.8*G.TIMERS.REAL) + 0.00*math.sin((G.TIMERS.REAL - math.floor(G.TIMERS.REAL))*math.pi*14)*(1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL)))^3
+            local rotate_mod = 0.05*math.sin(1.219*G.TIMERS.REAL) + 0.00*math.sin((G.TIMERS.REAL)*math.pi*5)*(1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL)))^2
+    
+            if self.blueprint_copy_key == 'j_hologram' then
+                self.hover_tilt = self.hover_tilt*1.5
+                self.children.floating_sprite:draw_shader('hologram', nil, self.ARGS.send_to_shader, nil, self.children.center, 2*scale_mod, 2*rotate_mod)
+                self.hover_tilt = self.hover_tilt/1.5
+            else
+                self.children.floating_sprite:draw_shader('dissolve',0, nil, nil, self.children.center,scale_mod, rotate_mod,nil, 0.1 + 0.03*math.sin(1.8*G.TIMERS.REAL),nil, 0.6)
+                self.children.floating_sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, scale_mod, rotate_mod)
+            end
+            --self.children.floating_sprite:draw_shader('dissolve',   0, nil, nil, self.children.center, scale_mod, rotate_mod, nil, 0.1 + 0.03 * math.sin(1.8 * G.TIMERS.REAL), nil, 0.6)
+            --self.children.floating_sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, scale_mod, rotate_mod)
+        end
+    end,
+    conditions = { vortex = false, facing = 'front', blueprint_sprite_copy = true },
+}
+
+SMODS.current_mod.config_tab = function()
+    return {n = G.UIT.ROOT, config = {r = 0.1, align = "cm", padding = 0.1, colour = G.C.BLACK, minw = 8, minh = 6}, nodes = {
+        {n = G.UIT.R, config = {align = "cl", padding = 0}, nodes = {
+            {n = G.UIT.C, config = { align = "c", padding = 0 }, nodes = {
+                { n = G.UIT.T, config = { text = "Brainstorm", scale = 0.35, colour = G.C.UI.TEXT_LIGHT }},
+            }},
+            {n = G.UIT.C, config = { align = "cl", padding = 0.05 }, nodes = {
+                create_toggle{ col = true, label = "", scale = 0.85, w = 0, shadow = true, ref_table = Blueprint.SETTINGS, ref_value = 'brainstorm' },
+            }},
+        }},
+        {n = G.UIT.R, config = {align = "cl", padding = 0}, nodes = {
+            {n = G.UIT.C, config = { align = "c", padding = 0 }, nodes = {
+                { n = G.UIT.T, config = { text = "Blueprint", scale = 0.35, colour = G.C.UI.TEXT_LIGHT }},
+            }},
+            {n = G.UIT.C, config = { align = "cl", padding = 0.05 }, nodes = {
+                create_toggle{ col = true, label = "", scale = 0.85, w = 0, shadow = true, ref_table = Blueprint.SETTINGS, ref_value = 'blueprint' },
+            }},
+        }},
+    }}
+end
